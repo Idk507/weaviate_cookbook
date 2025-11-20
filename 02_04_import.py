57b4c2b507aae5f83b05ea48752682a2175fde98
Dhanushkumar <93417785+Idk507@users.noreply.github.com>
2025-11-20 21:37:32 +0530

    Add files via upload

---
+++ 02_04_import.py
@@ -0,0 +1,35 @@
+import utils
+import weaviate.classes as wvc
+from weaviate.util import generate_uuid5
+import pandas as pd
+
+movie_df = pd.read_csv("data/movies_data.csv")  # Load the data
+
+client = utils.connect_to_my_db()  # Connect to our own database
+movies = client.collections.get("Movie")   # Get the Movie collection
+
+
+movie_objs = list()
+for i, row in movie_df.iterrows():
+    props = {
+        "title": row["Movie Title"],
+        "description": row["Description"],
+        "rating": row["Star Rating"],
+        "director": row["Director"],
+        "movie_id": row["ID"],
+        "year": row["Year"],
+    }
+
+    movie_uuid = generate_uuid5(row["ID"])
+    data_obj = wvc.data.DataObject(
+        properties=props,
+        uuid=movie_uuid,
+    )
+    movie_objs.append(data_obj)
+
+response = movies.data.insert_many(movie_objs)
+
+print(f"Insertion complete with {len(response.all_responses)} objects.")
+print(f"Insertion errors: {len(response.errors)}.")
+
+client.close()
